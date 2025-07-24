98f06a113b19bfffa59c6ebd460042ca019d735a
Paul-202425 <p.masamvu@alustudent.com>
2025-07-25 00:38:24 +0200

    output check solved

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,14 +2,13 @@
 """ top_ten.py """
 import requests
 
-
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    """Prints the titles of the first 10 hot posts listed in a subreddit."""
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        print()
+        print("None")  # <-- This is what the assignment wants
         return
     posts = response.json()['data']['children']
     for post in posts:
