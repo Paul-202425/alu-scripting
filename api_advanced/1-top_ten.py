68a45d6de0e7e056fd02f18dc58faeb113dfe411
Paul-202425 <p.masamvu@alustudent.com>
2025-07-25 00:21:53 +0200

    output check solved

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,4 +1,8 @@
-def top_ten(subreddit):
+(#!/usr/bin/python3
+""" top_ten.py """
+import requests
+
+ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
