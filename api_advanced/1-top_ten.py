c951f365acc7101f06bc7effdad7ebf33bdea00c
Paul-202425 <p.masamvu@alustudent.com>
2025-07-25 00:51:02 +0200

    output check solved

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ top_ten.py """
 import requests
+
 def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print("None")
+
     else:
         posts = response.json()['data']['children']
         for post in posts:
