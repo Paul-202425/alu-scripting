716c27670400002758351b91a1261399e4545737
Paul-202425 <p.masamvu@alustudent.com>
2025-07-25 02:14:27 +0200

    output check solved

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,7 +2,6 @@
 """ top_ten.py """
 import requests
 
-
 def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'myRedditScript/0.1 by u/Due-Memory4378'}
@@ -10,9 +9,10 @@ def top_ten(subreddit):
     if response.status_code != 200:
         print("None")
         return
-    posts = response.json()['data']['children']
-    if posts:
-        for post in posts:
-            print(post['data']['title'])
-    else:
-        pass
+    posts = response.json().get('data', {}).get('children', [])
+    if not posts:
+        return  # Don't print anything for empty posts!
+    for post in posts:
+        title = post['data'].get('title')
+        if title:  # Extra guard
+            print(title)
