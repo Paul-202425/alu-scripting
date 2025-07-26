38030833ffed5301cb66fccec83f6c8a2f936e3f
Paul-202425 <p.masamvu@alustudent.com>
2025-07-26 23:50:59 +0200

    ammend 1-top_ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""Module for top_ten function"""
 import requests
 
 
 def top_ten(subreddit):
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'myRedditScript/0.1 by u/Due-Memory4378'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print("None")
-        return
-    posts = response.json().get('data', {}).get('children', [])
-    if not posts:
-        return  # Don't print anything for empty posts!
-    for post in posts:
-        title = post['data'].get('title')
-        if title:  # Extra guard
-            print(title)
+    """Function that queries the Reddit API."""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json().get('data').get('children')
+        for post in data:
+            print(post.get('data').get('title'))
+    else:
+        print(None)
